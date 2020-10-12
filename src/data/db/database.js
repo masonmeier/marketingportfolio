adfd94a2b81c182979540c522b3c46b844519612
masonmeier <57198981+masonmeier@users.noreply.github.com>
2020-10-12 16:07:21 -0700

    initial commit

---
+++ src/data/db/database.js
@@ -0,0 +1,519 @@
+import Mock from "../mock";
+
+const database = {
+  information: {
+    name: 'Mason Meier',
+    aboutContent: "I am a recently self taught full stack developer. I can provide clean code, hosting and database management. I can also generate custom graphics for interactive sites and applications.",
+    age: 25,
+    phone: '916-812-7103',
+    nationality: 'American',
+    language: 'English',
+    email: 'digitalmasonry2020@gmail.com',
+    address: '2609 Stonecreek Drive #16 Sacramento, CA 95833',
+    freelanceStatus: 'Available',
+    socialLinks: {
+      facebook: 'https://www.facebook.com/mason.meier.7',
+      twitter: 'https://twitter.com/Omnidingus',
+      pinterest: '',
+      behance: '',
+      linkedin: '',
+      dribbble: '',
+      github: 'https://github.com/masonmeier'
+    },
+    brandImage: '/images/brand-image.jpg',
+    aboutImage: '/images/about-image.jpg',
+    aboutImageLg: '/images/about-image-lg.jpg',
+    cvfile: '/files/mason-meier.pdf'
+  },
+  services: [
+    {
+      title: "Web Design",
+      icon: 'color-pallet',
+      details: "I can provide both front end design and functionality utilizing HTML, CSS, Photoshop and a plethora of other design tools."
+    },
+    {
+      title: "Web Development",
+      icon: 'code',
+      details: "I can create and manage application functionality utilising Javascript and React."
+    },
+    {
+      title: "Mobile First Design",
+      icon: 'mobile',
+      details: "I have a firm understanding of mobile first design and can generate applications that will function on any screen size."
+    }
+  ],
+  reviews: [
+    {
+      id: 1,
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita impedit nobis tempore quaerat quibusdam, aliquid maxime tempora.",
+      author: {
+        name: 'Burdette Turner',
+        designation: 'Web Developer, Abc Company'
+      }
+    },
+    {
+      id: 2,
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita impedit nobis tempore quaerat quibusdam.",
+      author: {
+        name: 'Susan Yost',
+        designation: 'Client'
+      }
+    },
+    {
+      id: 3,
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      author: {
+        name: 'Irving Feeney',
+        designation: 'Fiverr Client'
+      }
+    }
+  ],
+  skills: [
+    {
+      title: "HTML5",
+      value: 90
+    },
+    {
+      title: "CSS4",
+      value: 70
+    },
+    {
+      title: "Javascript",
+      value: 70
+    },
+    {
+      title: "jQuery",
+      value: 60
+    },
+    {
+      title: "ReactJS",
+      value: 70
+    },
+    {
+      title: "Bash",
+      value: 70
+    },
+    {
+      title: "AWS",
+      value: 60
+    },
+    {
+      title: "Research and Analysis",
+      value: 80
+    },
+    {
+      title: "Photoshop",
+      value: 65
+    },
+    {
+      title: "Marketing Strategies",
+      value: 80
+    },
+  ],
+  portfolios: [
+    {
+      id: 1,
+      title: "Where-to",
+      subtitle: "An application that helps the user determine what nation in the world most closely aligns with their " +
+        "personal values.",
+      imageUrl: "/images/portfolio-image-1.jpg",
+      largeImageUrl: ["/images/portfolio-image-1-lg.jpg"],
+      url: 'https://where-to.world'
+    },
+    {
+      id: 2,
+      title: "Fitguard Inc",
+      subtitle: "This project is still in progress. " +
+        "Link is not yet available.",
+      imageUrl: "/images/portfolio-image-4.jpg",
+      largeImageUrl: [
+        "/images/portfolio-image-4-lrg.jpg",
+        "/images/portfolio-image-4-lrg.jpg"
+      ],
+      url: ''
+    },
+    // {
+    //   id: 3,
+    //   title: "Tea & Coffee Mug",
+    //   subtitle: "Beautiful mug with logo.",
+    //   imageUrl: "/images/portfolio-image-2.jpg",
+    //   url: 'https://pinterest.com'
+    // },
+    // {
+    //   id: 4,
+    //   title: "Pen Holder",
+    //   subtitle: "A pen holder with beautiful design.",
+    //   imageUrl: "/images/portfolio-image-3.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-3-lg.jpg"],
+    //   url: '#'
+    // },
+    // {
+    //   id: 5,
+    //   title: "Mug",
+    //   subtitle: "Mug with awesome style",
+    //   imageUrl: "/images/portfolio-image-5.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-5-lg.jpg"]
+    // },
+    // {
+    //   id: 6,
+    //   title: "Pendrive",
+    //   subtitle: "Free pendrive mockup design.",
+    //   imageUrl: "/images/portfolio-image-6.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-6-lg.jpg"]
+    // },
+    // {
+    //   id: 7,
+    //   title: "Beautiful Pendrive",
+    //   subtitle: "Pendrive with great design & flexible.",
+    //   imageUrl: "/images/portfolio-image-7.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-7-lg.jpg"],
+    //   url: 'https://twitter.com'
+    // },
+    // {
+    //   id: 8,
+    //   title: "Sticker",
+    //   subtitle: "Clip sticker mockup design.",
+    //   imageUrl: "/images/portfolio-image-8.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-8-lg.jpg"]
+    // },
+    // {
+    //   id: 9,
+    //   title: "Packet",
+    //   subtitle: "Beautiful packet & product design.",
+    //   imageUrl: "/images/portfolio-image-9.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-9-lg.jpg"]
+    // },
+    // {
+    //   id: 10,
+    //   title: "Pen Holder",
+    //   subtitle: "A pen holder with beautiful design.",
+    //   imageUrl: "/images/portfolio-image-3.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-3-lg.jpg"],
+    //   url: '#'
+    // },
+    // {
+    //   id: 11,
+    //   title: "Coffee Mug",
+    //   subtitle: "Awesome coffee mug design.",
+    //   imageUrl: "/images/portfolio-image-4.jpg",
+    //   largeImageUrl: [
+    //     "/images/portfolio-image-4-lg.jpg",
+    //     "/images/portfolio-image-4-lg2.jpg"
+    //   ],
+    //   url: 'https://facebook.com'
+    // },
+    // {
+    //   id: 12,
+    //   title: "Tea & Coffee Mug",
+    //   subtitle: "Beautiful mug with logo.",
+    //   imageUrl: "/images/portfolio-image-2.jpg",
+    //   url: 'https://pinterest.com'
+    // },
+    // {
+    //   id: 13,
+    //   title: "T-shirt Mockup",
+    //   subtitle: "A beautiful t-shirt mockup.",
+    //   imageUrl: "/images/portfolio-image-1.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-1-lg.jpg"],
+    //   url: 'https://dribbble.com'
+    // },
+    // {
+    //   id: 14,
+    //   title: "Mug",
+    //   subtitle: "Mug with awesome style",
+    //   imageUrl: "/images/portfolio-image-5.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-5-lg.jpg"]
+    // },
+    // {
+    //   id: 15,
+    //   title: "Pendrive",
+    //   subtitle: "Free pendrive mockup design.",
+    //   imageUrl: "/images/portfolio-image-6.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-6-lg.jpg"]
+    // },
+    // {
+    //   id: 16,
+    //   title: "Beautiful Pendrive",
+    //   subtitle: "Pendrive with great design & flexible.",
+    //   imageUrl: "/images/portfolio-image-7.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-7-lg.jpg"],
+    //   url: 'https://twitter.com'
+    // },
+    // {
+    //   id: 17,
+    //   title: "Sticker",
+    //   subtitle: "Clip sticker mockup design.",
+    //   imageUrl: "/images/portfolio-image-8.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-8-lg.jpg"]
+    // },
+    // {
+    //   id: 18,
+    //   title: "Packet",
+    //   subtitle: "Beautiful packet & product design.",
+    //   imageUrl: "/images/portfolio-image-9.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-9-lg.jpg"]
+    // },
+    // {
+    //   id: 19,
+    //   title: "T-shirt Mockup",
+    //   subtitle: "A beautiful t-shirt mockup.",
+    //   imageUrl: "/images/portfolio-image-1.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-1-lg.jpg"],
+    //   url: 'https://dribbble.com'
+    // },
+    // {
+    //   id: 20,
+    //   title: "Coffee Mug",
+    //   subtitle: "Awesome coffee mug design.",
+    //   imageUrl: "/images/portfolio-image-4.jpg",
+    //   largeImageUrl: [
+    //     "/images/portfolio-image-4-lg.jpg",
+    //     "/images/portfolio-image-4-lg2.jpg"
+    //   ],
+    //   url: 'https://facebook.com'
+    // },
+    // {
+    //   id: 21,
+    //   title: "Tea & Coffee Mug",
+    //   subtitle: "Beautiful mug with logo.",
+    //   imageUrl: "/images/portfolio-image-2.jpg",
+    //   url: 'https://pinterest.com'
+    // },
+    // {
+    //   id: 22,
+    //   title: "Pen Holder",
+    //   subtitle: "A pen holder with beautiful design.",
+    //   imageUrl: "/images/portfolio-image-3.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-3-lg.jpg"],
+    //   url: '#'
+    // },
+    // {
+    //   id: 23,
+    //   title: "Mug",
+    //   subtitle: "Mug with awesome style",
+    //   imageUrl: "/images/portfolio-image-5.jpg",
+    //   largeImageUrl: ["/images/portfolio-image-5-lg.jpg"]
+    // }
+  ],
+  experience: {
+    workingExperience: [
+      {
+        id: 1,
+        year: "2017-2020",
+        position: "Marketing Director",
+        company: "Fitguard Inc.",
+        details: "Established a regional monopoly over all Northern California territories by establishing and leveraging client relationships and creating cyclical growth practices. " +
+          "Secured long term state and federal level government contracts. Worked directly with Fortune 500 companies. Generated an expansive network of contacts throughout multiple" +
+          " sectors of California's industries."
+      },
+      {
+        id: 2,
+        year: "2015 - 2018",
+        position: "Installation Team Lead",
+        company: "Fitguard Inc.",
+        details: "Organized and managed multiple sets of on site machine installation teams. Handled all customer facing interactions for both the installation teams and sales representatives."
+      },
+    ],
+    educationExperience: [
+      {
+        id: 1,
+        year: "2019 - Present",
+        graduation: "Self Taught",
+        university: "Thinkful Full Stack Software Engineering Boot Camp +",
+        details: "Began learning software engineering to assist in modernizing Fitguard Inc. Worked directly with multiple mentors developing skills and projects in: HTML5, CSS4, Javascript, jQuery, React," +
+          "AWS and Linux."
+      },
+      {
+        id: 2,
+        year: "2009 - 2013",
+        graduation: "High School Graduate",
+        university: "Natomas Charter School's Performing and Fine Arts Academy",
+        details: "Graduated with digital arts accolades. Recognized by the school district for perfect scores on California Standardized Testing. Team lead on background animation for Source production. " +
+          "Created installation galleries and stop motion productions. Worked tinting black and white frames for A Trip To the Moon."
+      },
+    ]
+  },
+  blogs: [
+    {
+      id: 1,
+      title: 'Markdown & Html supported blog.',
+      featuredImage: '/images/blog-image-1.jpg',
+      filesource: '../../blog/markdown-html-supported-blog.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 2,
+      title: 'Installing NodeJS on your device.',
+      featuredImage: '/images/blog-image-2.jpg',
+      filesource: '../../blog/installing-nodejs-on-your-device.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 3,
+      title: 'UI/UX design starter with Adobe XD.',
+      featuredImage: '/images/blog-image-3.jpg',
+      filesource: '../../blog/uiux-design-starter-with-adobe-xd.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 4,
+      title: 'Boost your post for increasing sales.',
+      featuredImage: '/images/blog-image-4.jpg',
+      filesource: '../../blog/boost-your-post-for-increasing-sales.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 5,
+      title: 'Difference between GatsbyJS & NextJS.',
+      featuredImage: '/images/blog-image-5.jpg',
+      filesource: '../../blog/difference-between-gatsbyjs-and-nextjs.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 6,
+      title: 'How to choose javascript framework for project.',
+      featuredImage: '/images/blog-image-6.jpg',
+      filesource: '../../blog/how-to-choose-javascript-framework-for-project.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 7,
+      title: 'Web automation with python language.',
+      featuredImage: '/images/blog-image-7.jpg',
+      filesource: '../../blog/web-automation-with-python-language.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 8,
+      title: 'Time to use latest technology for creating a website.',
+      featuredImage: '/images/blog-image-8.jpg',
+      filesource: '../../blog/time-to-use-latest-technology-for-creating-a-website.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 9,
+      title: 'Think out of the box.',
+      featuredImage: '/images/blog-image-9.jpg',
+      filesource: '../../blog/think-out-of-the-box.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 10,
+      title: 'Trending designs in 2020.',
+      featuredImage: '/images/blog-image-1.jpg',
+      filesource: '../../blog/trending-designs-in-2020.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 11,
+      title: 'How to get 10k instagram followers?',
+      featuredImage: '/images/blog-image-2.jpg',
+      filesource: '../../blog/how-to-get-10k-instagram-followers.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 12,
+      title: 'What NodeJS can do?',
+      featuredImage: '/images/blog-image-3.jpg',
+      filesource: '../../blog/what-nodejs-can-do.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 13,
+      title: 'Futures of javascript.',
+      featuredImage: '/images/blog-image-4.jpg',
+      filesource: '../../blog/future-of-javascript.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 14,
+      title: 'Popular javascript library in 2020.',
+      featuredImage: '/images/blog-image-5.jpg',
+      filesource: '../../blog/popular-javascript-library-in-2020.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    },
+    {
+      id: 15,
+      title: 'Promrammers must read books.',
+      featuredImage: '/images/blog-image-6.jpg',
+      filesource: '../../blog/programmers-must-read-books.md',
+      createDay: "20",
+      createMonth: 'February',
+      createYear: "2020"
+    }
+  ],
+  contactInfo: {
+    phoneNumbers: ['9168127103'],
+    emailAddress: ['digitalmasonry2020@gmail.com'],
+    address: "Sacramento, Ca"
+  }
+};
+
+
+Mock.onGet("/api/information").reply(config => {
+  const response = database.information;
+  return [200, response];
+});
+
+Mock.onGet("/api/services").reply(config => {
+  const response = database.services;
+  return [200, response];
+});
+
+Mock.onGet("/api/reviews").reply(config => {
+  const response = database.reviews;
+  return [200, response];
+});
+
+Mock.onGet("/api/skills").reply(config => {
+  const response = database.skills;
+  return [200, response];
+});
+
+Mock.onGet("/api/portfolios").reply(config => {
+  const response = database.portfolios;
+  return [200, response];
+});
+
+Mock.onGet("/api/experience").reply(config => {
+  const response = database.experience;
+  return [200, response];
+});
+
+Mock.onGet("/api/blog").reply(config => {
+  const response = database.blogs;
+  return [200, response];
+});
+
+Mock.onGet("/api/contactinfo").reply(config => {
+  const response = database.contactInfo;
+  return [200, response];
+});
